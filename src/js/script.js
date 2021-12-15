7ad91945fcc6f0d7054e66f5326ee92d3cba64fc
YuraKogut <kogutyura9@gmail.com>
2021-12-15 22:31:43 +0200

    done

--- src/js/script.js
+++ src/js/script.js
@@ -1,43 +1,19 @@
-const quotes = [
-	{ 
-		"quote" : "The only sin is ignorance", 
-		"source" : "Christopher Marlowe" 
-	},
-	{
-		"quote" : "A man is his own easiest dupe, for what he wishes to be true he generally believes to be true", 
-		"source" : "Demosthenes"
-	},
-	{
-		"quote" : "A lie can travel halfway around the world while the truth is putting on its shoes", 
-		"source" : "Mark Twain"
-	},
-	{
-		"quote" : "Great minds discuss ideas; average minds discuss events; small minds discuss people", 
-		"source" : "Eleanor Roosevelt"
-	},
-	{
-		"quote" : "If you have a garden and a library, you have everything you need", 
-		"source" : "Marcus Tullius Cicero"
-	},
-	{
-		"quote" : "Truth comes out in wine", 
-		"source" : "Pliny the Elder"
-	},
-	{
-		"quote" : "Everything in the universe is within you. Ask all from yourself", 
-		"source" : "Rumi"
-	},
-	{
-		"quote" : "When I get a little money I buy books; and if any is left I buy food and clothes", 
-		"source" : "Desiderius Erasmus"
-	}
-]
 
 function randomQuote() {
-  let random = quotes[Math.floor(Math.random() * quotes.length)];
-  quotation.innerText = `“${random.quote}.”`;
+  fetch('https://api-phrase.herokuapp.com/getPhrase')
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+   document.getElementById('quotation').innerHTML=JSON.stringify(data)
+  }).catch(error =>{
+  	console.log(error);
+  })
+
 }
 
 randomQuote();
 
 document.querySelector("button").addEventListener('click', randomQuote)
+
+
