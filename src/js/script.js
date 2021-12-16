a2da18bd0447f5ab62fc8bb2353ad94373674257
YuraKogut <kogutyura9@gmail.com>
2021-12-16 20:12:37 +0200

    /

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,18 @@
 function randomQuote() {
 	//зчитуємо дані з сервера(ссилка)
-  fetch('https://api-phrase.herokuapp.com/getPhrase?count=1')
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
+	fetch('https://api-phrase.herokuapp.com/getPhrase?count=1')
+	.then((response) => {
+		return response.json();
+	})
+	.then((data) => {
   	//звернення до масива(об"єкта")
   	let myString = JSON.stringify(data[0].text);
   	//звертаємось до параграфа
-   document.getElementById('quotation').innerHTML=myString;
+  	document.getElementById('quotation').innerHTML=myString;
   })//для помилки
-  .catch(error =>{
-  	console.log(error);
-  })
+	.catch(error =>{
+		console.log(error);
+	})
 }
 
 // виводимо функцію
@@ -24,10 +24,10 @@ document.querySelector("button").addEventListener('click', randomQuote)
 $( document ).ready(function(){
 	  $( ".button" ).click(function(){ // задаєм функцію при ннатисканні на елемент с классом button
 	    $( ".block-generator" ).fadeOut(); // плавно змінюємо прозорість та  приховуємо  всі елементи <div>
-	  });
+	});
 	  $( ".button" ).click(function(){ // задаєм функцію при ннатисканні на елемент с классом button
 	    $( ".block-generator" ).fadeIn(2000); // плавно змінюємо прозорість та  відображаємо  всі елементи <div>
-	  });
+	});
 	});
 
 $( document ).ready(function(){
