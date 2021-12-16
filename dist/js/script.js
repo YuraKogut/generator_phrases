4821b137aa7c296ac525b96782dc399d2acc206e
YuraKogut <kogutyura9@gmail.com>
2021-12-16 15:16:28 +0200

    /

--- dist/js/script.js
+++ dist/js/script.js
@@ -22,22 +22,22 @@ document.querySelector("button").addEventListener('click', randomQuote)
 
 
 $( document ).ready(function(){
-	  $( ".button" ).click(function(){ // задаем функцию при нажатиии на элемент с классом out
+	  $( ".button" ).click(function(){ // задаем функцию при нажатиии на элемент с классом button
 	    $( ".block-generator" ).fadeOut(); // плавно изменяя прозрачность скрываем все элементы <div>
 	  });
-	  $( ".button" ).click(function(){ // задаем функцию при нажатиии на элемент с классом in
+	  $( ".button" ).click(function(){ // задаем функцию при нажатиии на элемент с классом button
 	    $( ".block-generator" ).fadeIn(2000); // плавно изменяя прозрачность отображаем все элементы <div>
 	  });
 	});
 
 $( document ).ready(function(){
-	  $( ".button" ).click(function(){ // задаем функцию при нажатиии на элемент с классом out
+	  $( ".button" ).click(function(){ // задаем функцию при нажатиии на элемент с классом button
 	      $( ".lds-ring" ).removeClass('active').delay(100).fadeOut(300) // плавно изменяя прозрачность скрываем все элементы <div>
 	  });
-	   $( ".button" ).click(function(){ // задаем функцию при нажатиии на элемент с классом out
+	   $( ".button" ).click(function(){ // задаем функцию при нажатиии на элемент с классом button
 	      $( ".lds-ring" ).addClass('active').delay(100).fadeIn(300) // плавно изменяя прозрачность скрываем все элементы <div>
 	  });
-	    $( ".button" ).click(function(){ // задаем функцию при нажатиии на элемент с классом out
+	    $( ".button" ).click(function(){ // задаем функцию при нажатиии на элемент с классом button
 	      $( ".lds-ring" ).removeClass('active').delay(100).fadeOut(300) // плавно изменяя прозрачность скрываем все элементы <div>
 	  });
 	});
