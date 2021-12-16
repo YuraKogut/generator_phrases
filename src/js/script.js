001a8a5526870e32ac36a71342c824b88e2220a4
YuraKogut <kogutyura9@gmail.com>
2021-12-16 02:47:59 +0200

    generator has been done

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,23 @@
 function randomQuote() {
+	//зчитуємо дані з сервера(ссилка)
   fetch('https://api-phrase.herokuapp.com/getPhrase?count=1')
   .then((response) => {
     return response.json();
   })
   .then((data) => {
+  	//звернення до масива(об"єкта")
   	let myString = JSON.stringify(data[0].text);
+  	//звертаємось до параграфа
    document.getElementById('quotation').innerHTML=myString;
-  }).catch(error =>{
+  })//для помилки
+  .catch(error =>{
   	console.log(error);
   })
 
 }
+// виводимо функцію
 randomQuote();
-
+//клік обробляємо
 document.querySelector("button").addEventListener('click', randomQuote)
 
 
